a08d2be39e149078055278ab4589033afbf9837e
saori <z36.noda.saori@gmail.com>
2018-09-20 19:08:24 +0900

    グループ管理、メッセージ機能

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,12 +2,12 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
   has_many :messages
-  # accepts_nested_attributes_for :group_user
+
   validates :name, presence: true
 
   def show_last_message
     if (last_message = messages.last).present?
-      last_message.content? ? last_message.content : '画像が投稿されています'
+      last_message.text? ? last_message.text : '画像が投稿されています'
     else
       'まだメッセージはありません。'
     end
