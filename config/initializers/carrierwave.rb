c2aafedeaafeb59d8594a25966d7d7eb38499047
saori <z36.noda.saori@gmail.com>
2018-09-29 21:09:47 +0900

    Change bk

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chatspace1'
-  config.asset_host = 'https://s3-ap-northeast-1ap-northeast-1.amazonaws.com/chatspace1'
+  config.fog_directory  = 'chatspacesaori'
+  config.asset_host = 'https://s3-ap-northeast-1ap-northeast-1.amazonaws.com/chatspacesaori'
 end
